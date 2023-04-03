d6f2baf5ccfc21843545e02823de96ef92e5b2e6
atharv soni <er.viveksoni90@gmail.com>
2023-04-03 19:11:42 +0530

    Implement Edit Button in Todo

--- GUI.py
+++ GUI.py
@@ -1,15 +1,17 @@
 import Functions_File as ff
 import PySimpleGUI as ps
 
+ps.theme("DarkTeal2")
+
 AddButton = ps.Button("Add")
 Lable1= ps.Text("Enter a To-Do")
-InputText1= ps.InputText(tooltip="Enter your todo here",expand_x=True)
+InputText1= ps.InputText(tooltip="Enter your todo here",size=(45),key="IT1")
 
-InputText2= ps.InputText(tooltip="Your To-Do List",expand_x=True,expand_y=True)
-ShowButton=ps.Button("Show")
+ListBox1= ps.Listbox(values=ff.get_todos(),enable_events=True,size=(44,10),key="Lb1")
+EditButton=ps.Button("Edit")
 ExitButton=ps.Button("Exit")
 window=ps.Window("To-Do Application",
-                 layout=[[Lable1],[InputText1,AddButton],[InputText2,ShowButton],[ExitButton]],
+                 layout=[[Lable1],[InputText1,AddButton],[ListBox1,EditButton],[ExitButton]],
                  size=(800,500),
                  font=("Helvetica",20))
 
@@ -24,9 +26,9 @@ while True:
     match event:
         case 'Add'|'add':
             todos=ff.get_todos()
-            todos.append(values[0]+'\n')
-            print(todos)
+            todos.append(values["IT1"]+'\n')
             ff.write_todos(todos)
+            window["Lb1"].update(values=todos)
 
         case ps.WIN_CLOSED:
             break
@@ -34,9 +36,17 @@ while True:
         case 'Exit':
             break
 
-        case 'Show'|'display':
+        case 'Edit'|'edit':
+            todo = values['Lb1'][0]
+            new_todo = values["IT1"]
             todos = ff.get_todos()
+            index=todos.index(todo)
+            todos[index]=new_todo+'\n'
+            ff.write_todos(todos)
+            window['Lb1'].update(values=todos)
 
+        case 'Lb1':
+            window['IT1'].update(value=values["Lb1"][0])
 
 
 window.close()
