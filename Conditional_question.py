8eab0dff51335e59b2263393ed4aa709f7718046
atharv soni <er.viveksoni90@gmail.com>
2023-04-03 17:15:41 +0530

    Changes

--- Conditional_question.py
+++ Conditional_question.py
@@ -2,5 +2,4 @@
 '''Input word ends with a/A, print in upper case'''
 ''' Input word start with ga/GA ends with 'SH, swap the case'''
 '''Word ends with a and len is 4, lower the case and reverse'''
-'' \
-
+''
