5ac463b6bbd174996880803498fa86ffab7230fb
atharv soni <er.viveksoni90@gmail.com>
2023-04-04 13:17:22 +0530

    Added Delete Functionality and enhancements to strip spaces. Adding new project of File_Browser

--- GUI.py
+++ GUI.py
@@ -5,13 +5,14 @@ ps.theme("DarkTeal2")
 
 AddButton = ps.Button("Add")
 Lable1= ps.Text("Enter a To-Do")
-InputText1= ps.InputText(tooltip="Enter your todo here",size=(45),key="IT1")
+InputText1= ps.InputText(tooltip="Enter your todo here",size=(45),key="IT1",do_not_clear=False)
 
 ListBox1= ps.Listbox(values=ff.get_todos(),enable_events=True,size=(44,10),key="Lb1")
 EditButton=ps.Button("Edit")
 ExitButton=ps.Button("Exit")
+DeleteButton=ps.Button("Delete")
 window=ps.Window("To-Do Application",
-                 layout=[[Lable1],[InputText1,AddButton],[ListBox1,EditButton],[ExitButton]],
+                 layout=[[Lable1],[InputText1,AddButton],[ListBox1,EditButton],[DeleteButton],[ExitButton]],
                  size=(800,500),
                  font=("Helvetica",20))
 
@@ -25,10 +26,14 @@ while True:
     print(values)
     match event:
         case 'Add'|'add':
-            todos=ff.get_todos()
-            todos.append(values["IT1"]+'\n')
-            ff.write_todos(todos)
-            window["Lb1"].update(values=todos)
+            if(len(values['IT1'])!=0):
+                todos=ff.get_todos()
+                todos.append(values["IT1"].strip()+'\n')
+                ff.write_todos(todos)
+                window["Lb1"].update(values=todos)
+
+            else:
+                ps.popup("Todo cannot be Blank")
 
         case ps.WIN_CLOSED:
             break
@@ -46,7 +51,14 @@ while True:
             window['Lb1'].update(values=todos)
 
         case 'Lb1':
-            window['IT1'].update(value=values["Lb1"][0])
+            window['IT1'].update(value=values["Lb1"][0].strip())
 
+        case 'Delete':
+            todos=ff.get_todos()
+            index =todos.index(values['Lb1'][0])
+            print(values['Lb1'][0])
+            todos.pop(index)
+            ff.write_todos(todos)
+            window['Lb1'].update(values=todos)
 
 window.close()
