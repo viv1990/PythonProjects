582352940fdd190e72bfda505f018538adc2810f
atharv soni <er.viveksoni90@gmail.com>
2023-04-06 15:28:27 +0530

    Flie_Browser_Compressor Project Completed

--- File_Browser_Comressor.py
+++ File_Browser_Comressor.py
@@ -9,9 +9,9 @@ lable1 = ps.Text("Enter files to compress")
 lable2 = ps.Text("Enter Destination")
 InputBox1 = ps.InputText(tooltip="File Log",key='FL',do_not_clear=False)
 InputBox2 = ps.InputText(tooltip="Destination File Log",key='DF', do_not_clear=False)
-OutputLabel = ps.Text(key='Output')
+OutputLabel = ps.Text(key="OL")
 
-layout = [[lable1],[InputBox1,Browse],[lable2],[InputBox2,DestFolder],[Compress]]
+layout = [[lable1],[InputBox1,Browse],[lable2],[InputBox2,DestFolder],[Compress],[OutputLabel]]
 
 
 window = ps.Window("File Compressor",layout=layout)
@@ -19,12 +19,14 @@ list1=[]
 while True:
     event, values = window.read()
     print(values)
+    for element in window.AllKeysDict:
+        ps.Print(f"Element Key: {element}")
     if event =='Compress':
         filepaths = values["Browse"].split(';')
         print(filepaths)
         folder=values['DF']
         zipfile_function.make_zip(filepaths,folder)
-        window['Output'].update("Compression Completed")
+        window["OL"].update(value="Compression Completed")
 
     if event==ps.WIN_CLOSED:
         break
